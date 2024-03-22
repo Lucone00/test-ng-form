98531336867f63bdadfa2b2d1cbba8f13854c0fa
Lucone00 <luca.giuntoli2001.b@gmail.com>
2024-03-22 11:58:42 +0000

    base form creata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-ng-form';
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = this.fb.group({
+      currency: ['', Validators.required],
+      choice: ['', Validators.required],
+      date: ['', Validators.required],
+      text: ['', Validators.required],
+      integer: ['', Validators.required],
+      float: ['', Validators.required],
+      quantity: ['', Validators.required],
+      price: ['', Validators.required],
+    });
+  }
+
+  calculateTotal(): number {
+    const quantity = this.form.get('quantity')?.value ?? 0;
+    const price = this.form.get('price')?.value ?? 0;
+    return quantity * price;
+  }
+
+  onSubmit() {
+    console.log('Form submitted');
+    console.log(this.form.value);
+  }
 }
